980d198f56e8149c8e6edbca8ae5aa517cfa9594
Tejkumar1 <vipintej7797@gmail.com>
2025-12-30 16:30:50 +0530

    Update application configuration and main Spring Boot setup

--- src/main/java/com/expensetracker/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/expensetracker/expense_tracker/ExpenseTrackerApplication.java
@@ -8,10 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpenseTrackerApplication.class, args);
+		SpringApplication.run(ExpenseTrackerApplication.class, args)
 	}
-
+       
 }
-
-
 
